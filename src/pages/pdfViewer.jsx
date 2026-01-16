aafdf91989226c703900aebe0bb1c5bc3098da3b
AdamFariello <ajf270@scarletmail.rutgers.edu>
2026-01-15 21:30:27 -0500

    Updated pdfViewer, commiting before putting files into folder

--- src/pages/pdfViewer.jsx
+++ src/pages/pdfViewer.jsx
@@ -1,8 +1,18 @@
 import PdfRender from "./pdfRender"
 
 export default function pdfViewer() {
+    function Setings() {
+
+    }
+
     return(<>
         <h1>Reading, "Building a Second Brain"</h1>
-        <PdfRender />
+        
+        <button>Settings</button>
+
+        <div style={{display:"flex"}} > 
+            <PdfRender />
+            <PdfRender />
+        </div>
     </>)
 }
