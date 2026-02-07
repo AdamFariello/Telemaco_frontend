4196b74df765b66d3653ef7afa3b03995666e215
AdamFariello <ajf270@scarletmail.rutgers.edu>
2026-02-06 21:11:53 -0500

    Forgot to remove .epubs, will bloat the repo

--- src/pages/epub/readingPage.jsx
+++ src/pages/epub/readingPage.jsx
@@ -1,10 +1,13 @@
 import EpubGenerator from "./epubGenerator";
 
+const epub_under5mb = "./example.epub";
+const epub_over5mb = "./tooBigExample.epub";
+
 export default function ReadingPage() {
     return (<>
         <h1>Example to highlight in comparison</h1>
         <div style={{display:"flex", flexDirection:"row"}}>
-            <EpubGenerator epubLink="./example.epub"/>
+            <EpubGenerator epubLink={epub_over5mb}/>
             <EpubGenerator epubLink="./example.epub"/>
         </div>
     </>);
